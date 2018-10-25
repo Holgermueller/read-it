1277aa2fd590702e1198bac3ea20030eb2921736
Holger Mueller <9jholger@gmail.com>
2018-10-25 13:34:50 -0500

    header formatted

--- public/templates/header.php
+++ public/templates/header.php
@@ -7,26 +7,28 @@
     <title>Read-It!</title>
 <!--styles-->
 <link rel="stylesheet" href="./css/styles.css">
+<link href="https://fonts.googleapis.com/css?family=Charmonman:700" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 <link type="text/plain" rel="author" href="http://read-it/humans.txt">
 </head>
 <body>
 
     <div class="header">
         <div class="brand">
-            <img src="#" alt="Logo Here!" class="logo"> 
-            <h1 class="app-name">
+            <h1 class="app-name header-elem">
+            <i class="fas fa-book"></i>
                 Read It!
-            </h1>
+            </h1> 
+
+            <form method="post" class="log-in-form header-elem">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+                <label for="password">Password</label>
+                <input type="text" name="password" id="password">
+                <input type="submit" name="login" value="Log In" class="log-in">
+            </form>
+
         </div>
-
-    <form method="post" class="log-in-form">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username">
-        <label for="password">Password</label>
-        <input type="text" name="password" id="password">
-        <input type="submit" name="login" value="Log In" class="log-in">
-    </form>
-
     </div>
     
 </body>
