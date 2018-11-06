01f9872f7cb6d0fece52871250e422d3e560542f
Holger Mueller <9jholger@gmail.com>
2018-11-05 20:07:26 -0600

    scaffold delete user page

---
+++ public/delete-account.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * function to delete a user account
+ */
+
+
+ /**
+  * function to return user to his or her profile
+  */
+
+
+?>
+
+<?php include "templates/header.php"; ?>
+
+<h2>Are you sure you want to delete your account?</h2>
+
+<button type="button">Yes</button>
+
+<button type="button">No</button>
+
+<?php include "templates/footer.php"; ?>
